5c19c93c1c2d9800f7da6c7ff352b6fbfd258dad
Jay Vegad <143251488+Vegadjay@users.noreply.github.com>
2024-12-01 09:26:33 +0530

    initial commit

---
+++ controllers/authController.js
@@ -0,0 +1,66 @@
+const bcrypt = require('bcrypt');
+const generateToken = require('../utils/generateTokens')
+const userModel = require("../models/user-model")
+
+const regiseterUser = async (req,res) => {
+    const { fullName, email, password } = req.body;
+    
+    try {
+        const existingUser = await userModel.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ message: "User already exists" });
+        }
+
+        const salt = await bcrypt.genSalt(parseInt(process.env.SALT_COUNT));
+        const hash = await bcrypt.hash(password, salt);
+
+        const user = await userModel.create({
+            fullName,
+            email,
+            password: hash,
+        });
+
+        let token = generateToken(user);       
+
+        res.cookie("token", token, { 
+            httpOnly: true, 
+            secure: process.env.NODE_ENV === 'production',
+            sameSite:true 
+        });
+
+        res.status(201).json({ 
+            message: "User created successfully",
+            userId: user._id 
+        });
+
+    } catch(err) {
+        console.error(err); 
+        res.status(500).json({ 
+            message: "Server Error", 
+            error: err.message 
+        });
+    }
+}
+
+const loginUser = async (req,res) => {
+    const { email,password } = req.body;
+    try {
+        const user = await userModel.findOne({email: email});
+        if(!user) {
+            res.status(500).send("Email Or Password is incorrect ! ");
+            console.log("If i hit")
+        }
+        else {
+            bcrypt.compare(password, user.password, function(err,result) {
+                res.send(result);
+            });
+        }
+        } catch (err) {
+            res.send(err);
+        }
+}
+
+module.exports = {
+    regiseterUser,
+    loginUser
+};
