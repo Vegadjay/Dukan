1f52cec79b0727d3348d95d864fa5aec4234affd
Jay Vegad <143251488+Vegadjay@users.noreply.github.com>
2024-12-21 09:55:04 +0530

    Update __test_-

--- __tests__/.test.js
+++ __tests__/.test.js
@@ -1,5 +1,6 @@
-describe('Example Test', () => {
-    test('should pass', () => {
-        expect(true).toBe(true);
+// __tests__/sample.test.js
+describe('Sample Test', () => {
+    test('2 plus 2 equals 4', () => {
+        expect(2 + 2).toBe(4);
     });
 });
