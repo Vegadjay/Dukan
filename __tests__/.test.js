107b13d2a907e9ecb8c8275cecfe68cdfe440c65
Jay Vegad <143251488+Vegadjay@users.noreply.github.com>
2024-12-21 10:09:35 +0530

    Change yml

--- __tests__/.test.js
+++ __tests__/.test.js
@@ -1,6 +1,4 @@
-// __tests__/sample.test.js
-describe('Sample Test', () => {
-    test('2 plus 2 equals 4', () => {
-        expect(2 + 2).toBe(4);
-    });
+// __tests__/basic.test.js
+test('Basic test', () => {
+    expect(true).toBe(true);
 });
