5bebd5ab0c1e190e190fbd0efec0c4e9bc065993
Jay Vegad <143251488+Vegadjay@users.noreply.github.com>
2024-12-21 23:55:07 +0530

    Update Envs

--- config/mongooseConnection.js
+++ config/mongooseConnection.js
@@ -1,15 +1,18 @@
 const mongoose = require("mongoose");
-const config = require('config');
 const debug = require("debug")('development:mongoose');
 require("dotenv").config();
 
-
 const connectMongodb = async () => {
     try {
-        await mongoose.connect(`${config.get("MONGODB_URI")}/scatch`).then(() => {
-        })
+        const mongoUri = process.env.MONGODB_URI || config.get("MONGODB_URI");
+        await mongoose.connect(mongoUri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        debug("MongoDB connected successfully");
     } catch (err) {
-        console.log(err)
+        console.error("MongoDB connection error:", err);
+        process.exit(1); // Exit process with failure
     }
 }
 
